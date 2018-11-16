cc6591d681bb5193a7d3dfb23d8dba23d91f25b9
Alex Beams <beamsa1@hawkmail.newpaltz.edu>
2018-11-16 16:40:05 -0500

    Portrait functionality, menu and nav editions, more photos and design work

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const portrait = document.querySelector(".portrait");
 
 // Set Initial State of Menu
 let showMenu = false;
@@ -16,6 +17,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
+    portrait.classList.add("move");
     navItems.forEach(item => item.classList.add("show"));
 
     //Set Menu State
@@ -25,6 +27,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
+    portrait.classList.remove("move");
     navItems.forEach(item => item.classList.remove("show"));
 
     //Set Menu State
